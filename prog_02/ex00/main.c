bfd8500d2a9d428358384c41573e97f11b6aab2a
celeloup <celia.m.leloup@gmail.com>
2022-01-18 12:13:00 +0100

    restored the missing interrupt function and modified to work on new button position

--- prog_02/ex00/main.c
+++ prog_02/ex00/main.c
@@ -12,17 +12,34 @@ int main()
     DDRB = (1 << PB3);
 
     // Set External Interrupt Control Register A to generate interrupt request
-    // on any logical change of INT1
-    EICRA |= (1 << ISC10);
+    // on any logical change of INT0 (button on pin PD2 on board week 2)
+    EICRA |= (1 << ISC00);
 
     // Set External Interrupt Mask Register
-    EIMSK |= (1 << INT1);
+    EIMSK |= (1 << INT0);
 
     // Enable global interrupt
     SREG |= (1 << 7);
 
+
     for(;;)
     {}
-        
+
     return (0);
+}
+
+int input = 0;
+
+// Function called on interrupt
+__attribute__((signal)) void INT0_vect()
+{
+    if (input == 0)
+    {
+        PORTB ^= (1 << PB3);
+        input = 1;
+    }
+    else
+        input = 0;
+    for (long i = 0; i < DEBOUCE_DELAY; i++)
+    {}
 }
