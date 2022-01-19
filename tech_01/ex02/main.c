3e8f68ad804aeab663619b79379df11736435439
celeloup <celia.m.leloup@gmail.com>
2022-01-19 18:15:38 +0100

    turning back in time

--- tech_01/ex02/main.c
+++ tech_01/ex02/main.c
@@ -3,7 +3,6 @@
 #endif
 
 #include <avr/io.h>
-#include <stdlib.h>
 
 // Program to change RGB led color on button push (but complicated colors now)
 // OC0A ROUGE
@@ -11,83 +10,8 @@
 // OC2B BLEU
 # define DEBOUCE_DELAY 50000
 
-void uart_init(uint8_t baud)
-{
-    // SET BAUD RATE
-    UBRR0H = (baud >> 8);  // Most significant bit
-    UBRR0L = baud;         // Least significant bit
-
-    UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);    // Set frame format: 8data (Table 20-11, p.203)
-    UCSR0B |= (1 << TXEN0) | (1 << RXEN0);     // Enable  transmitter (20.11.3 p.202)
-}
-
-void uart_tx(char c)
-{
-    // Wait for the transmit buffer to be ready to receive data
-    while (!(UCSR0A & (1 << UDRE0)));
-    UDR0 = c;   // Set buffer
-}
-
-char uart_rx(void)
-{
-	while (!(UCSR0A & (1 << RXC0)));    // Wait for data to be received
-	return UDR0;    // Get and return received data from buffer
-}
-
-void uart_printstr(char *str)
-{
-    int i = 0;
-    while (str[i])
-    {
-        uart_tx(str[i]);
-        i++;
-    }
-}
-
-char input[256] = "";
-int in = 0;
-
-void parse_cmd()
-{
-    // if (input[0] == "#")
-    char red_s[3] = {input[1], input[2], '\0'};
-    char green_s[3] = {input[3], input[4], '\0'};
-    char blue_s[3] = {input[5], input[6], '\0'};
-    OCR0A = (uint8_t)strtol(red_s, NULL, 16);;
-    OCR0B = (uint8_t)strtol(green_s, NULL, 16);
-    OCR2B = (uint8_t)strtol(blue_s, NULL, 16);
-    PORTB ^= (1 << PB3);
-    input[0] = 0;
-    in = 0;
-}
-
-__attribute__((signal)) void USART_RX_vect()
-{
-    char c = UDR0;
-    if (c == 127 && in > 0)
-    {
-        input[in] = '\0';
-        in--;
-        uart_printstr("\b \b");
-    }
-    else if (c == 13)
-    {
-        input[in] = '\0';
-        uart_tx('\n');
-        uart_tx('\r');
-        parse_cmd();
-    }
-    else if (in < 255 && c != 127)
-    {
-        input[in++] = c;
-        uart_tx(c);
-    }
-}
-
 int main()
-{
-    uart_init(8);
-    
+{   
     // LEDS
             //  R           G             B
     DDRD = (1 << PD6) | (1 << PD5) | (1 << PD3);
@@ -110,7 +34,6 @@ int main()
     TCCR2B |= (1 << CS22);
     OCR2B = 0xff;
 
-    UCSR0B |= (1 << RXCIE0); // Receive complete interrupt flag
     SREG = (1 << 7);
 
     // BUTTON
@@ -138,9 +61,9 @@ int main()
                     i = 0;
                 }
             }
-			for (long i = 0; i < DEBOUCE_DELAY; i++)
+            for (long i = 0; i < DEBOUCE_DELAY; i++)
             {}
-			last_state = new_state;
+            last_state = new_state;
         }
     }
     return (0);
